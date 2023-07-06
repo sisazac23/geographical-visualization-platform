0816538dfb4ea42768545e5a2f3480bfab15a54c
sisazac <sisazac23@gmail.com>
2023-07-06 18:25:53 +0000

    Added missions map

--- Home.py
+++ Home.py
@@ -20,32 +20,21 @@ st.set_page_config(layout="wide")
 
 st.title("GeoData Visualization Platform")
 
-
-
-
-
 # load dataframe and information
 def load_data(path):
     df = gpd.read_file(path)
     return df
 
-# load geojson
-#with open('zoned_data_sit.geojson') as f:
-#    geo= json.load(f)
-
-
-
 def threshold(data):
     threshold_scale = np.linspace(data.min(), data.max(), 6, dtype=float)
-    threshold_scale[-1] = float(threshold_scale[-1]) + 1
+    #threshold_scale[-1] = float(threshold_scale[-1]) + 1
     return threshold_scale.tolist()
 
-
-def show_map(data,data_geo,threshold_scale,variable):
+def show_map(data, data_geo, threshold_scale, variable):
     maps = folium.Choropleth(
         geo_data=data_geo,
         data=data,
-        columns=['zone',variable],
+        columns=['zone', variable],
         threshold_scale=threshold_scale,
         key_on='feature.id',
         fill_color='YlOrRd',
@@ -63,30 +52,67 @@ def show_map(data,data_geo,threshold_scale,variable):
     folium_static(stream_map)
 
 
-map_dict = {'Concentrations map': 'data/zoned_data_sit.geojson', 'Chemicals map': 'data/zoned_chem_sit.geojson'}
+def plot_points(data, size=10000, color='red'):
+    stream_map = folium.Map(location=[6.2518400, -75.5635900], zoom_start=10, control_scale=True, tiles=select_tile_provider,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+    for i in range(len(data)):
+        location = [data.iloc[i].geometry.y, data.iloc[i].geometry.x]
+        folium.CircleMarker(location=location, radius=5, color=color, fill=True, fill_color=color).add_to(stream_map)
 
-select_map = st.sidebar.selectbox('Select map', ['Concentrations map', 'Chemicals map'])
+    folium.LayerControl().add_to(stream_map)
+    
+    folium_static(stream_map)
 
-df = load_data(map_dict[select_map])
+
+
+
+tile_providers = {
+    'OpenStreetMap': 'OpenStreetMap',
+    'Stamen Terrain': 'Stamen Terrain',
+    'Stamen Toner': 'Stamen Toner',
+    'CartoDB Positron': 'CartoDB Positron',
+    'CartoDB Dark_Matter': 'CartoDB Dark_Matter',
+}
+
+map_dict = {'Concentrations map': 'data/zoned_data_sit.geojson', 'Chemicals map': 'data/zoned_chem_sit.geojson',
+            'Metal risk map': 'data/zoned_metalic_sit.geojson','Mission map 1':'data/mission1.geojson','Mission map 2':'data/mission2.geojson'}
+
+
+select_tile_provider = st.sidebar.selectbox('Select tile provider', list(tile_providers.keys()))
+select_map = st.sidebar.selectbox('Select map', ['Concentrations map', 'Chemicals map', 'Metal risk map', 'Missions'])
+
+
+if select_map == 'Missions':
+    select_mission = st.sidebar.selectbox('Select mission', ['Mission map 1', 'Mission map 2'])
+    df = load_data(map_dict[select_mission])
+    select_variable = st.sidebar.selectbox('Select variable', list(df.columns[~df.columns.isin(['geometry','lat','lot'])]))
+    df = df[[select_variable, 'geometry','lat','lot']]
 
 if select_map == 'Concentrations map':
+    df = load_data(map_dict[select_map])
     select_variable = st.sidebar.selectbox('Select variable', list(df.columns[~df.columns.isin(['geometry', 'zone'])]))
-    df = df[[select_variable,'zone', 'geometry']]
+    df = df[[select_variable, 'zone', 'geometry']]
 
 if select_map == 'Chemicals map':
+    df = load_data(map_dict[select_map])
     select_variable = st.sidebar.selectbox('Select variable', list(df.columns[~df.columns.isin(['geometry', 'zone'])]))
-    df = df[[select_variable,'zone', 'geometry']]
+    df = df[[select_variable, 'zone', 'geometry']]
+
+if select_map == 'Metal risk map':
+    df = load_data(map_dict[select_map])
+    select_variable = 'risk'
+    df = df[[select_variable,'geometry','zone']]
+
+if select_map != 'Missions':
+    stream_map = folium.Map(location=[6.2518400, -75.5635900], zoom_start=10, control_scale=True, tiles=select_tile_provider,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+    show_map(df, df, threshold(df[select_variable]), select_variable)
+else:
+    #stream_map = folium.Map(location=[6.2518400, -75.5635900], zoom_start=10, control_scale=True, tiles=select_tile_provider,locate_control=True, latlon_control=True, draw_export=True, minimap_control=True)
+    plot_points(df)
+    # plot variable in y axis, x axix is the index
+    st.line_chart(df[select_variable])
+    
 
 
-
-stream_map = folium.Map(location=[6.2518400, -75.5635900], zoom_start=10, control_scale=True)
-show_map(df,df,threshold(df[select_variable]),select_variable)
-
-st.sidebar.title("About")
 
 logo = "logo-eafit.png"
 st.sidebar.image(logo)
-
-
-# cito - exposición - supervivencia
-
