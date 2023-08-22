e443d5b3fc4f5c4448b928bf81fac087f5360ce7
sisazac23 <sisazac23@gmail.com>
2023-08-22 22:43:34 +0000

    Added download data and export report functions

--- Home.py
+++ Home.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import streamlit as st
 import geopandas as gpd
+import matplotlib.pyplot as plt
 import folium 
 import numpy as np
+from fpdf import FPDF
+import base64
+from tempfile import NamedTemporaryFile
 from streamlit_folium import folium_static
+import plotly.graph_objects as go
+
 
 import os
+import io
 
 
 st.set_page_config(layout="wide")
@@ -25,9 +32,18 @@ def load_data(path):
     df = gpd.read_file(path)
     return df
 
+def save_geojson_with_bytesio(dataframe):
+    #Function to return bytesIO of the geojson
+    shp = io.BytesIO()
+    dataframe.to_file(shp,  driver='GeoJSON')
+    return shp
+
+def create_download_link(val, filename):
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+
 def threshold(data):
     threshold_scale = np.linspace(data.min(), data.max(), 6, dtype=float)
-    #threshold_scale[-1] = float(threshold_scale[-1]) + 1
     return threshold_scale.tolist()
 
 def show_map(data, data_geo, threshold_scale, variable):
@@ -107,22 +123,67 @@ if select_map != 'Missions':
     show_map(df, df, threshold(df[select_variable]), select_variable)
 else:
     plot_points(df)
-    
+    figs = []
     st.markdown(f"## {select_variable} Line Chart")
     # beautify the st.line_chart(df[select_variable], use_container_width=True) plot
     st.line_chart(df[select_variable], use_container_width=True)
+    # plot using plotly
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df.index, y=df[select_variable], mode='lines+markers'))
+    figs.append(fig)
     st.markdown(f"## {select_variable} Histogram")
+    # plot using plotly
+    fig = go.Figure()
+    fig.add_trace(go.Histogram(x=df[select_variable]))
+    figs.append(fig)
     st.bar_chart(df[select_variable], use_container_width=True)
     # center the descriptive statistics markdown st.markdown(f"## {select_variable} Descriptive Statistics")
     st.markdown(f"## {select_variable} Descriptive Statistics")
+    # create fig using plotly
     st.table(df[select_variable].describe().round(2).transpose())
+    fig = go.Figure(data=[go.Table(
+        header=dict(values=['Metric', 'Value'],
+                    fill_color='paleturquoise',
+                    align='left'),
+        cells=dict(values=[list(df[select_variable].describe().round(2).index),
+                           list(df[select_variable].describe().round(3).values)],
+                   fill_color='lavender',
+                   align='left'))
+    ])
+    figs.append(fig)
 
 
+
+if select_map != 'Missions':
+    st.download_button(
+        label="Download data",
+        data=save_geojson_with_bytesio(df),
+        file_name=str(select_map)+str(select_variable)+'.geojson',
+        mime='application/geo+json',
+    )
+else:
+    st.download_button(
+        label="Download data",
+        data=save_geojson_with_bytesio(df),
+        file_name=str(select_map)+str(select_mission)+str(select_variable)+'.geojson',
+        mime='application/geo+json',
+    )
+
+if select_map == 'Missions':
+    export_as_pdf = st.button("Export Report")
+    if export_as_pdf:
+        pdf = FPDF()
+        for fig in figs:
+            pdf.add_page()
+            with NamedTemporaryFile(delete=False, suffix=".png") as tmpfile:
+                    fig.write_image(tmpfile.name)
+                    pdf.image(tmpfile.name, 10, 10, 200, 100)
+        html = create_download_link(pdf.output(dest="S").encode("latin-1"), "Report_"+str(select_mission)+'_'+str(select_variable))
+        st.markdown(html, unsafe_allow_html=True)
        
 st.sidebar.title("About")
 markdown = "Platform to visualize chemical measurements over different colombian points of interest" 
 st.sidebar.info(markdown)
 logo = "logo-eafit.png"
 st.sidebar.image(logo)
-
 
